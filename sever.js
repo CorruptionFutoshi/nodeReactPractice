f9fc0e3c7cc06a40ca7cf9eb30f9da3fb40e2468
CorruptionFutoshi <corruptionf13@gmail.com>
2023-09-13 16:56:56 +0900

    @一回目

---
+++ sever.js
@@ -0,0 +1,14 @@
+const http=require('node:http');
+const port=8000;
+const html=require('fs').readFileSync('index.html');
+
+const server = http.createServer((req,res)=>{
+	res.statusCode=200;
+	res.setHeader('Content-Type','text/html;charset=UTF-8');
+	res.write(html);
+	res.end();
+});
+
+server.listen(port, ()=>{
+	console.log(`Server running at Port:${port}`);
+});
