ccc613655af237cb8e48b0df20bfaff88bfa9c33
CorruptionFutoshi <corruptionf13@gmail.com>
2023-09-15 22:53:34 +0900

    useStateRikai

--- test/src/App.js
+++ test/src/App.js
@@ -1,16 +1,45 @@
 import TodoList from "./TodoList";
-import { useState} from "react";
+import { useState,useRef} from "react";
+import {v4 as uuidv4} from "uuid";
 
 function App() {
-	const [todos,setTodos] =useState(["Todo1","Todo2"]);	
+	const [todos,setTodos] =useState([ ]);
+	
+	const todoNameRef=useRef();
+	
+	const handleAddTodo=()=>{
+		const name=todoNameRef.current.value;
+			
+		if(name===""){
+			return;
+		}
+
+		setTodos((prevTodos)=>
+			{
+				return [...prevTodos, {id:uuidv4(), name:name, completed:false}];
+			});
+		todoNameRef.current.value=null;
+	};
+
+	const handleRemoveTodo=()=>{
+		const todosCopy=todos.filter((todo)=>!todo.completed);
+		setTodos(todosCopy);
+	};
+	
+	const toggleTodoCompleted=(id)=>{
+		const todosCopy=[...todos];
+		const target=todosCopy.find((todoCopy)=>todoCopy.id===id);
+		target.completed=!target.completed;
+		setTodos(todosCopy);
+	}
 
 	return (
 		<>
-		<TodoList todos={todos}/>
-		<input type="text" />
-		<button>タスクの追加</button>
-		<button>タスクの削除</button>
-		<div>残りタスク：０</div>	
+		<TodoList todos={todos} toggleTodoCompleted={toggleTodoCompleted}/>
+		<input type="text" ref={todoNameRef}/>
+		<button onClick={handleAddTodo}>タスクの追加</button>
+		<button onClick={handleRemoveTodo}>タスクの削除</button>
+		<div>残りタスク：{todos.filter((todo)=>!todo.completed).length}</div>	
 
 		</>
 	);
--- test/src/Todo.js
+++ test/src/Todo.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-const Todo =(props) =>{
-	return <div>{props.todo}</div>;
+const Todo =({todo,toggleTodoCompleted}) =>{
+	const handleCheckBoxClick=()=>{
+		toggleTodoCompleted(todo.id);
+	};
+
+	return [
+	 <div>
+	  <label>	
+	   <input type="checkbox" checked={todo.completed} onChange={handleCheckBoxClick} readOnly/>
+	  </label>
+	  {todo.name}
+
+	 </div>
+	];
 };
 
 export default Todo;
